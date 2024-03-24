4dbd6829b836d76929b29362bbf47fb033ec23d3
Tyler Fedrick <tyefedrick@gmail.com>
2024-03-24 17:42:39 -0400

    Only primerica.com accounts can create an account now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validate :must_be_primerica_email
+  
   has_many :user_roles
   has_many :roles, through: :user_roles
 
@@ -11,4 +13,11 @@ class User < ApplicationRecord
 
   has_many :downloaded_pdfs
   has_many :pdfs, through: :downloaded_pdfs
+
+  private
+
+  def must_be_primerica_email
+    domain = email.split('@').last.downcase if email.present?
+    errors.add(:email, 'must be a primerica.com account') unless domain == 'primerica.com'
+  end
 end
